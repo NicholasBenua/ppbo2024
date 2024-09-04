f1c0ab33344eca919dcb6687300568147ee2ba80
NicholasBenua <benuaputra406@gmail.com>
2024-09-05 02:00:22 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -4,7 +4,11 @@ class Lingkaran
 {
     const PHI = 3.14;
     public $jari_jari;
-    
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function luas() : float {
         return self::PHI*$this->jari_jari*$this->jari_jari;
     }
@@ -19,6 +23,10 @@ class Bola
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function volume() : float {
         return (4/3)*self::PHI*pow($this->jari_jari,3);
     }
@@ -30,6 +38,11 @@ class Tabung
     public $jari_jari;
     public $tinggi;
     
+    public function __construct($jari_jari, $tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
@@ -41,30 +54,25 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
 
+    public function __construct($jari_jari, $tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return (1/3)*self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
-$objLingkaran = new Lingkaran();
-$objLingkaran->jari_jari = 9;
-
+$objLingkaran = new Lingkaran(13);
 echo "Luas Lingkaran = {$objLingkaran->luas()}cm^2\n";
 echo "Keliling Lingkaran = {$objLingkaran->keliling()}cm\n";
 
-$objBola = new Bola();
-$objBola->jari_jari = 6;
-
+$objBola = new Bola(9);
 echo "Volume Bola = {$objBola->volume()}cm^3\n";
 
-$objTabung = new Tabung();
-$objTabung->jari_jari = 7;
-$objTabung->tinggi = 14;
-
+$objTabung = new Tabung(8, 13);
 echo "Volume Tabung = {$objTabung->volume()}cm^3\n";
 
-$objKerucut = new Kerucut();
-$objKerucut->jari_jari = 9;
-$objKerucut->tinggi = 15;
-
+$objKerucut = new Kerucut(5, 15);
 echo "Volume Kerucut = {$objKerucut->volume()}cm^3\n";
