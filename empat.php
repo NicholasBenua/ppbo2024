6dcad4d6f92d24d2f4ce498ea88d8292d86a4441
NicholasBenua <benuaputra406@gmail.com>
2024-09-06 16:01:38 +0700

    Praktikum 5B sampai 5E Penggunaan Require dan Include

--- empat.php
+++ empat.php
@@ -1,96 +1,9 @@
 <?php
 
-use function PHPSTORM_META\type;
+require_once 'Author.php';
+require_once 'Book.php';
+require_once 'Publisher.php';
 
-class Author
-{
-    public $name;
-    public $description;
-
-    public function __construct($name, $description) {
-        $this->name = $name;
-        $this->description = $description;
-    }
-
-    public function show($type) {
-        return array(
-            'type' => $type,
-            'name' => $this->name,
-            'description' => $this->description
-        );
-    }
-}
-
-class Book
-{
-  public $ISBN;
-  public $title;
-  public $description;
-  public $category;
-  public $language;
-  public $numberOfPage;
-  public $author;
-  public $publisher;
-
-  public function __construct($ISBN, $title, $description, $category, $language, $numberOfPage, $author, $publisher)
-  {
-    $this->ISBN = $ISBN;
-    $this->title = $title;
-    $this->description = $description;
-    $this->category = $category;
-    $this->language = $language;
-    $this->numberOfPage = $numberOfPage;
-    $this->author = $author;
-    $this->publisher = $publisher;
-  }
-
-  public function showAll()
-  {
-    return array (
-        'ISBN' => $this->ISBN,
-        'title' => $this->title,
-        'description' => $this->description,
-        'category' => $this->category,
-        'language' => $this->language,
-        'numberOfPage' => $this->numberOfPage,
-        'author' => $this->author,
-        'publisher' => $this->publisher,
-    );
-  }
-  public function detail($ISBN)
-  {
-    if($this->ISBN == $ISBN){
-        return $this->showAll();
-    }
-    else{
-        return array();
-    }
-  }
-}
-    
-    class Publisher
-{
-    public $name;
-    public $address;
-    private $phone;
-
-    public function __construct($name, $address, $phone)
-    {
-        $this->name = $name;
-        $this->address = $address;
-        $this->phone = $phone;
-    }
-
-    public function setPhone($phone) 
-    {
-        $this->phone = $phone;
-    }
-    public function getPhone()
-    {
-        return $this->phone;
-    }
-}
- 
 $author = new Author('Miguel de Cervantes', 'Miguel de Cervantes Saavedra is a Spanish novelist, poet and dramatic. 
 His name is best known through his novel Don Quixote de la Mancha, which is considered by many to be the first modern novel,
 one of the greatest works in Western Literature, and the largest in Spanish.');
