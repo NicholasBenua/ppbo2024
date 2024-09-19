9b9c4c50eb88f61193d92dc83a4a8d94ae816bf8
NicholasBenua <benuaputra406@gmail.com>
2024-09-19 09:30:39 +0700

    Praktikum 6C - Memisahkan Class di Dalam Folder Masing-masing

---
+++ App/Admin/ManajemenArtikel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Admin;
+class ManajemenArtikel
+{
+    public function tambah() : void
+    {
+        echo "Artikel berhasil ditambahkan";
+    }
+}
---
+++ App/Guest/HomePage.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Guest;
+class HomePage
+{
+    public function tampil() : void
+    {
+        echo "Halaman depan guest";
+    }
+}
